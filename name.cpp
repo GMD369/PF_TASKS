40292a15f4b0f0e1360a905361dfd9531526f697
MOHIYUDIN <ghulammohiyudin11@gmail.com>
2023-09-16 19:48:45 +0500

    tasks

---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main(){
+ system("color 46");
+
+cout<< "     MY NAME IS GHULAM MOHIYU UD DIN."<<endl;
+system("color 57");
+cout<< "     MY FATHER NAME IS MUHAMMAD NAWAZ."<<endl;
+}
